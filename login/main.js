41ae918121cefe2ea1b57fc69e1b3f8e520be75d
kiriapis <85273237+kiriapis@users.noreply.github.com>
2021-06-23 14:30:24 +0900

    Create main.js

---
+++ login/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#submit").click(function() {
+        var email = $.trim($("#email").val());
+        var password = $.trim($("#password").val());
+        
+        if (email == "") {
+            alert("이메일을 입력해주세요.");
+            $("#email").focus();
+        } else if (password == "") {
+            alert("비밀번호를 입력해주세요.");
+            $("#password").focus();
+        } else {
+            alert("이메일: " + email);
+            alert("비밀번호: " + password);
+        }
+    });
+});
